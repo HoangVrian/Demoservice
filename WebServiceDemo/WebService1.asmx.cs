94a1497f477bcd02c593ebce0c3c6464aded4637
hoangdat <garogolsdtorm284@gmail.com>
2019-11-06 10:23:19 +0700

    jajaj

---
+++ WebServiceDemo/WebService1.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceDemo
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public float convertToVND(int moneyDollar)
+        {
+            return 22000 * moneyDollar;
+        }
+        [WebMethod]
+        public float convertToUSD(int moneyVNdong)
+        {
+            return moneyVNdong / 22000;
+        }
+    }
+}
